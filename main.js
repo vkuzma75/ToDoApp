f0d0b6d8350c4e69379f587d4d879add1667783f
Vladimir <vladimir.kuzmanoski@gmail.com>
2022-02-25 06:32:40 +0100

    toDoApp

---
+++ main.js
@@ -0,0 +1,160 @@
+let todosArr = [];
+let editingMode = false;
+
+
+// Form variables
+const form = document.querySelector("#todoForm");
+const addButton = document.querySelector(".addButton");
+const todoInputName = document.querySelector("#todoName");
+const todoInputDate = document.querySelector("#todoDate");
+
+
+// Add ToDo
+form.addEventListener("submit", e => {
+    e.preventDefault();
+    if(editingMode) {
+      todoCard.todoName = todoInputName.value;
+      todoCard.todoDate = todoInputDate.value;
+      editingMode = false;
+      addButton.innerText = "Add ToDo";
+    } else {
+        const todoName = todoInputName.value;
+        const todoDate = todoInputDate.value;
+        // const date = new Date().toISOString();
+        const id = new Date().valueOf()
+        todo = {
+            id,
+            todoName,
+            isCompleted: false,
+            todoDate
+        }
+        todosArr = [todo, ...todosArr];
+    }
+    
+    localStorage.setItem("storageTodos", JSON.stringify(todosArr));
+    printTodo(todosArr);
+    todoInputName.value = "";
+    todoInputDate.value = "";
+}) 
+
+
+const todoList = document.querySelector(".todoList");
+
+function printTodo(array) {
+    todoList.innerHTML = ""
+    if(todosArr.length) {
+        array.forEach(({ id, todoName, todoDate, isCompleted }) => {
+         let li = document.createElement("li");
+         let actionsWrapper = document.createElement("div");
+         let deleteBtn = document.createElement("button")
+         let editBtn = document.createElement("button")
+         actionsWrapper.append(editBtn, deleteBtn);
+         actionsWrapper.classList.add("actionWrapper");
+         todoList.append(li);
+         li.setAttribute("id", id);
+         li.classList.add("liTodo")
+         let todoText = document.createElement("p");
+         todoText.classList.add("todoText");
+         let todoDateVal = document.createElement("p");
+         todoDateVal.innerText = todoDate;
+         todoDateVal.classList.add("todoText");
+         li.append(todoText, todoDateVal, actionsWrapper);
+         todoText.innerText = todoName;
+        //  deleteBtn.innerHTML = `<box-icon name='trash' class="deleteBtn"></box-icon>`;
+        //  editBtn.innerHTML = `<box-icon name='edit' class="editIcon"></box-icon>`;
+         deleteBtn.textContent = "Delete";
+         deleteBtn.classList.add('btn', 'deleteBtn');
+         editBtn.textContent = "Edit";
+         editBtn.classList.add('btn', 'editBtn')
+       
+    
+         if(isCompleted) {
+             li.classList.add("line-through");
+         }
+    
+         li.addEventListener("click", todoComplete);
+         deleteBtn.addEventListener("click", deleteTodo);
+         editBtn.addEventListener("click", editTodo);
+      })
+    } else {
+        noText()
+    }
+ 
+}
+
+
+// Delete todo
+function deleteTodo(e) {
+    e.stopPropagation()
+    const id = e.currentTarget.parentNode.parentNode.id
+    const finalCheck = confirm("Are you sure you want to delete todo?")
+    if(finalCheck) {
+        const todo = findTodo(id, todosArr);
+        const filteredArr = todosArr.filter(el => el.id !== todo.id);
+        todosArr = [...filteredArr];
+        printTodo(todosArr);
+        localStorage.setItem("storageTodos", JSON.stringify(todosArr));
+    }
+}
+
+// todo complete toggle
+function todoComplete(e) {
+ const id = e.currentTarget.id;
+ let todoCard = findTodo(id, todosArr);
+ todoCard.isCompleted = !todoCard.isCompleted;
+ printTodo(todosArr);
+ localStorage.setItem("storageTodos", JSON.stringify(todosArr));
+}
+
+// Edit todo
+function editTodo(e) {
+  e.stopPropagation()
+  editingMode = true;
+  const id = e.currentTarget.parentNode.parentNode.id;
+  addButton.innerText = "Update Todo";
+  todoCard = findTodo(id, todosArr)
+  todoInputName.value = todoCard.todoName;
+}
+
+
+// Separate function to find todo
+function findTodo(id, arr) {
+   const todo = arr.find(todo => todo.id === +id)
+   console.log(todo);
+   return todo;
+}
+
+
+// Getting Todos from storage on load
+window.addEventListener("load", (e) => {  
+    const todosFromStorage = localStorage.getItem("storageTodos")
+    if(todosFromStorage) {
+        const newTodosFromStorage = JSON.parse(todosFromStorage)
+        if(newTodosFromStorage.length > 0) {
+            todosArr = [...newTodosFromStorage];
+            printTodo(todosArr)
+        } else {
+            localStorage.removeItem("storageTodos");
+        }
+    }  else { 
+        noText()
+    }
+})
+
+// printing text if the list is empty
+function noText() {
+    const noItemsText = document.createElement("p")
+    todoList.append(noItemsText)
+    noItemsText.innerText = "There is no todos!";
+    noItemsText.classList.add("no-todos");
+}
+
+// Search by name
+
+let searchInput = document.querySelector("#searchTodo");
+
+searchInput.addEventListener("keyup", (e) => {
+    let query = e.currentTarget.value.toLowerCase();
+    let filteredSearchArr = todosArr.filter(el => el.todoName.toLowerCase().includes(query))
+    printTodo(filteredSearchArr);
+})
